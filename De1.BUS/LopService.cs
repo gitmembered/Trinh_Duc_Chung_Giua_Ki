bbcb95136ceb7fe738b6c3d6e722f1f2d82bd3b3
gitmembered <143337987+gitmembered@users.noreply.github.com>
2025-10-18 16:29:53 +0700

    Add files via upload
    .

---
+++ De1.BUS/LopService.cs
@@ -0,0 +1,11 @@
+﻿using System.Data;
+using De1.DAL;
+
+namespace De1.BUS
+{
+    public class LopService
+    {
+        SinhVienConnect dal = new SinhVienConnect();
+        public DataTable LayDSLop() => dal.GetAllLop();
+    }
+}
---
+++ De1.BUS/SinhvienService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Data;
+using De1.DAL;
+
+namespace De1.BUS
+{
+    public class SinhvienService
+    {
+        SinhVienConnect dal = new SinhVienConnect();
+
+        public DataTable LayDSSinhVien() => dal.GetAllSinhVien();
+        public void Them(string ma, string ten, string lop, DateTime ns) => dal.InsertSinhVien(ma, ten, lop, ns);
+        public void Sua(string ma, string ten, string lop, DateTime ns) => dal.UpdateSinhVien(ma, ten, lop, ns);
+        public void Xoa(string ma) => dal.DeleteSinhVien(ma);
+    }
+}
